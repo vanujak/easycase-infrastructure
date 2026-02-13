bd80500929a8de279d3bb5106a280445cf81e581
vanujak <vanujakofficial@gmail.com>
2026-02-13 11:45:02 +0530

    frontend port change

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,8 +21,8 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-    from_port   = 3002
-    to_port     = 3002
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
